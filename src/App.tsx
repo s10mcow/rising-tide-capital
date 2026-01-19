ecee0db2cb5f089f4df8206e4b259e572376e36c
Sten Muchow <sten.muchow@gmail.com>
2026-01-19 14:29:30 -0500

    chore: add click thru modals

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Suspense } from "react";
 import { LoadingScreen } from "@/components/LoadingScreen";
 import HomePage from "@/pages/Landing/Landing";
+import QRSourceModal from "@/components/QRSourceModal";
+import EmailSourceModal from "@/components/EmailSourceModal";
 
 export default function App() {
   return (
     <Suspense fallback={<LoadingScreen isLoading />}>
       <HomePage />
+      <QRSourceModal />
+      <EmailSourceModal />
     </Suspense>
   );
 }
